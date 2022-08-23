526af4d29e06d6f72a19305f346cc4222cf0111b
guican-China <110767627+guican-China@users.noreply.github.com>
2022-08-23 19:06:26 +0800

    Add files via upload

---
+++ JiedaoServlet.java
@@ -0,0 +1,34 @@
+package com.example.demo_web;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "JiedaoServlet", value = "/jd")
+public class JiedaoServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json;charset=utf-8");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+
+        BufferedReader reader =
+                new BufferedReader(
+                        new InputStreamReader(
+                                new FileInputStream(
+                                        new File("c:\\Engilsh\\xiaoqu.txt")
+                                )
+                        )
+                );
+
+        List<String> lines = new ArrayList<>();
+        String line ;
+        while((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+        String data="{\"地址\":上海市徐汇区虹桥路1号,\"名称\":港汇恒隆广场,\"户数\":11145\"}";
+        response.getWriter().println(data);
+    }
+}
